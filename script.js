5b10b5e85f92bb0eb133a3151c9b0849155aacdf
Mama`s soldier <159080981+Annaseer4002@users.noreply.github.com>
2024-12-26 20:47:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function  toggleAbout() {
+    const about = document.getElementById("info");
+
+    if (about.style.display ==="none") {
+        about.style.display="block";
+    } else {
+        about.style.display="none";
+    }
+}
