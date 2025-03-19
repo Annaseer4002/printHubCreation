10da266d945d191396599fa4186ffdc425fb90c6
Mama`s soldier <159080981+Annaseer4002@users.noreply.github.com>
2025-03-19 20:20:38 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function  toggleAbout() {
-    const about = document.getElementById("info");
-
-    if (about.style.display ==="none") {
-        about.style.display="block";
-    } else {
-        about.style.display="none";
-    }
-}
